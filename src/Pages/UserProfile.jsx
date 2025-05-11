df416568d8b6fba49a8b356992d61c8b45583d56
ZandileG <2562617@students.wits.ac.za>
2025-05-11 19:30:12 +0200

    user profile part 1

--- src/Pages/UserProfile.jsx
+++ src/Pages/UserProfile.jsx
@@ -1,10 +1,22 @@
 import React, { Fragment } from "react";
+import { useNavigate } from "react-router-dom";
+
 import Navbar from "../Components/Navbar";
 import Footer from "../Components/Footer";
 
+import UserImage from "../Images/User Icon.webp";
 import "../Styles/UserProfile.css";
 
 function UserProfile() {
+  const navigate = useNavigate();
+
+function yourRecipes(){
+    navigate("/your-recipes");
+  }
+
+function savedRecipes(){
+    navigate("/saved-recipes");
+  }
 
   return (
     <Fragment>
@@ -13,12 +25,19 @@ function UserProfile() {
     <header className="header"><nav><Navbar /></nav></header>
 
     <aside className="sidebar">
+    <section>
+      <img src={UserImage} alt="User" className="user-image" />
+    </section>
 
     </aside>
     
     <section className="content">
     <h1 className="page-heading">User Profile</h1>
 
+    <section className="tabs">
+    <section className="tab" onClick={yourRecipes}>Your Recipes</section>
+    <section className="tab" onClick={savedRecipes}>Saved Recipes</section>
+    </section>
     </section>
     <Footer />
     </main> 
